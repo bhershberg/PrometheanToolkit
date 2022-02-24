360777c3b1e1d57e4035e2f77fa72ed270ca701e
Benjamin Hershberg <benjamin@hershberg.com>
2022-02-23 20:16:19 -0800

    Update CB_FM_about.m

--- core/fileManager/callbacks/CB_FM_about.m
+++ core/fileManager/callbacks/CB_FM_about.m
@@ -1,15 +1,9 @@
 function CB_FM_about(source, event)
 
     msg = {};
-    msg{end+1} = 'Promethean Toolkit is a software tool for IC designers, by IC designers. It solves many of the most common problems encountered in physical testing (and design in CAD tools) by centralizing information describing the state of your system in a single source location. This centralization provides many advantages of synchronization, reproducibility, traceability, and usability. It is all handled through a graphical environment that boosts productivity and increases insight, enabling rapid test automation and multi-user collaboration.';
+    msg{end+1} = 'Promethean Toolkit is made for IC designers, by IC designers. It solves many of the most common problems encountered in design and testing of analog-mixed signal integrated circuits by centralizing the information the fully describes the state of your system. This centralization provides many advantages with regard to synchronization, reproducibility, traceability, and usability. It is all handled through a graphical environment and accompanying API that boosts productivity and increases insight, enabling rapid test automation and multi-user collaboration.';
     msg{end+1} = '';
-    msg{end+1} = '';
-    msg{end+1} = 'Promethean Toolkit was conceived and developed by Benjamin Hershberg at imec in Leuven, Belgium from 2018-2020.';
-    msg{end+1} = '';
-    msg{end+1} = 'Key continuing support is provided by:';
-    msg{end+1} = '    - The open-source community';
-    msg{end+1} = '    - Benjamin Hershberg';
-    msg{end+1} = '    - Imec';
+    msg{end+1} = 'This project was originally conceived and developed by Benjamin Hershberg at imec in Leuven, Belgium from 2018-2020. Continuing support is provided by open-source contributors on GitHub, Benjamin Hershberg, and Imec.';
     msg{end+1} = '';
     msg{end+1} = 'Get involved at: https://github.com/bhershberg/PrometheanToolkit';
     
