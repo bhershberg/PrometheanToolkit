e4d934030e86beaca319432b2ab0226d7efed0d9
Benjamin Hershberg <56002672+bhershberg@users.noreply.github.com>
2023-05-31 10:18:39 -0700

    Revert "extra sounds"
    This reverts commit b879d2bab10c59a86fc26aee259244678e1816f7.

--- core/common/TB_DoneSound.m
+++
@@ -1,3 +0,0 @@
-clc; close all; clear;
-
-DoneSound;
--- core/common/TB_Statusbar.m
+++
@@ -1,9 +0,0 @@
-clc; close all; clear;
-
-N = 40;
-Statusbarmsg = 'leave me alone';
-for ii=1:N
-    pause(0.5);
-    Statusbar;
-end
-Save2Results;
